6e407d35eeec6197cbe4c347e9fcf46a93d30688
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2024-01-16 18:04:50 -0800

    Completed but still not working

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
+import { ContactPicker } from '../contactPicker/ContactPicker';
 
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
-  return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  const [month, day, year] = new Date().toLocaleDateString('en-US').split('/');
+  return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
 };
 
 export const AppointmentForm = ({
@@ -17,10 +16,50 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
-
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="date">Date:</label>
+        <input
+          type="date"
+          id="date"
+          value={date}
+          min={getTodayString()}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="time">Time:</label>
+        <input
+          type="time"
+          id="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="contact">Contact:</label>
+        <ContactPicker
+          contacts={contacts}
+          id="contact"
+          value={contact}
+          onChange={(selectedContact) => setContact(selectedContact)}
+        />
+      </div>
+      <div>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
   );
 };
